cecd816e16a3f821b6b3a086fe2bd9adfc1eafa1
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-06 16:54:14 +0800

    feat(root): add header and subheader

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  <main class="container mx-auto my-8 space-y-8">Hello!</main>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h1 class="text-2xl font-medium">All Events</h1>
+    <h1 class="text-2xl font-medium">Your Bookings</h1>
+  </main>
 </template>
