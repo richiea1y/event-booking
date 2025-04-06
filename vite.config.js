be0795e404676813c6f3b8ad86f80b3d14e9c97e
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-06 16:08:29 +0800

    chore: remove useless files from scaffolding

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      // console.log(import.meta.url);
+      // output: file:///Users/richie/Project/LiteralResource/masterVue3/event-booking/vite.config.js
+
+      // new URL('./src', import.meta.url)：取得與當前模組相對的 ./src 資料夾的 URL。
+      // fileURLToPath：把這個 URL 轉換成系統可使用的絕對路徑字串（例如 /Users/richie/.../src）。
+
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
