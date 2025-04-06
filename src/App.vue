4f27b4bbe4aad4dbd0192435b62eac6695e13d4c
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-06 17:46:02 +0800

    feat(app): use v-for to render EventCard component dynamically

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <EventCard />
+    <EventCard v-for="i in 8" :key="i" />
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
