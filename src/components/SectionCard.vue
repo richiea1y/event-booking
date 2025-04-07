5b09bebc5db42af6efb460b41d21b805e3288c65
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-07 17:55:12 +0800

    feat(SectionCard): extract EventCard structure to SectionCard to create three name slot.

--- src/components/SectionCard.vue
+++ src/components/SectionCard.vue
@@ -1,5 +1,17 @@
 <template>
-  <RoundedCard></RoundedCard>
+  <RoundedCard>
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">
+      <slot name="header"></slot>
+    </h3>
+
+    <div class="p-4 border-b border-gray-200">
+      <slot></slot>
+    </div>
+
+    <div class="p-4">
+      <slot name="footer"></slot>
+    </div>
+  </RoundedCard>
 </template>
 
 <script setup>
