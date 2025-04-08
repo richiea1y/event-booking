7ecc8b7bf61bd02b972e8319e3cc1e8d560547cd
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-08 16:14:56 +0800

    feat(loading state): add flag that represent loading state and open at the begin of fetching and close at the end

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
       <EventCard
         v-for="event in events"
         :key="event.id"
@@ -12,6 +12,7 @@
         @register="console.log('Registered!')"
       />
     </section>
+    <section v-else>Loading events ...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-8">
       <BookingItem v-for="i in 3" :key="i" />
@@ -25,11 +26,16 @@ import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue'
 
 const events = ref([])
+const eventsLoading = ref(false)
 
 const fetchEvents = async () => {
-  const response = await fetch('http://localhost:3001/events')
-  events.value = await response.json()
-  console.log(events.value)
+  eventsLoading.value = ref(true)
+  try {
+    const response = await fetch('http://localhost:3001/events')
+    events.value = await response.json()
+  } finally {
+    eventsLoading.value = ref(false)
+  }
 }
 
 onMounted(() => fetchEvents())
