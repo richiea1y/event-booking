2d7b62e95b1539e01e168186c8bae589b851d42a
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-12 21:10:49 +0800

    refactor(composable): separate BookingList functionality from App
    - Move section block into the template of a BookingList
    - Move all unused import to script setup of BookingList
    - Move onMounted handler to BookingList
    - Move useBooking composable to BookingList
    - Add BookingList component to template of App

--- src/components/BookingList.vue
+++ src/components/BookingList.vue
@@ -1,3 +1,29 @@
-<template></template>
+<template>
+  <section class="grid grid-cols-1 gap-8">
+    <template v-if="!loading">
+      <BookingItem
+        v-for="booking in bookings"
+        :key="booking.id"
+        :title="booking.eventTitle"
+        :status="booking.status"
+        @cancelled="cancelBooking(booking.id)"
+      />
+    </template>
+    <template v-else>
+      <LoadingBookingItem v-for="i in 4" :key="i" />
+    </template>
+  </section>
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import BookingItem from '@/components/BookingItem.vue'
+import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
+import useBookings from '@/composables/useBookings.js'
+
+const { bookings, loading, fetchBookings, cancelBooking } = useBookings()
+
+onMounted(() => {
+  fetchBookings()
+})
+</script>
