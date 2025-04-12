946b80528effde7f8c57f1bcdc5e3bfe4486da64
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-12 20:56:30 +0800

    feat(composable): replace emit to composable then Now we don't need to handle the registration inside the App component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <EventList @register="handleRegistration($event)" />
+    <EventList />
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-8">
       <template v-if="!loading">
@@ -28,7 +28,7 @@ import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
 import EventList from '@/components/EventList.vue'
 import useBookings from '@/composables/useBookings.js'
 
-const { bookings, loading, fetchBookings, cancelBooking, handleRegistration } = useBookings()
+const { bookings, loading, fetchBookings, cancelBooking } = useBookings()
 
 onMounted(() => {
   fetchBookings()
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -17,7 +17,7 @@
             :title="event.title"
             :when="event.date"
             :description="event.description"
-            @register="$emit('register', event)"
+            @register="handleRegistration(event)"
           />
         </template>
         <template v-else>
@@ -36,8 +36,9 @@ import { ref, onMounted } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import LoadingEventCard from '@/components/LoadingEventCard.vue'
 import SectionCard from '@/components/SectionCard.vue'
+import useBookings from '@/composables/useBookings'
 
-defineEmits(['register'])
+const { handleRegistration } = useBookings()
 
 const events = ref([])
 const loading = ref(false)
