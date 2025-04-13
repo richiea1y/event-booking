08a291a8d177af7915c0cfb12bc5130ae9700f9f
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-13 14:34:08 +0800

    refactor(ErrorCard): To make SectionCard that show on template of EventList be reusable

---
+++ src/components/ErrorCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <SectionCard>
+    <div class="space-y-4 items-center flex flex-col">
+      <div class="text-red-500">Could not load events at the moment. Please try again.</div>
+      <RoundButton @click="fetchEvents">Retry now</RoundButton>
+    </div>
+  </SectionCard>
+</template>
+
+<script setup>
+import SectionCard from '@/components/SectionCard.vue'
+import RoundButton from '@/components/RoundButton.vue'
+</script>
