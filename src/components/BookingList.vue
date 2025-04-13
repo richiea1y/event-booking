b6737cb645cbfa3b0b5dea71edb4de399c99b703
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-13 14:50:28 +0800

    feat(exercise6): add the error handling to the BookingList

--- src/components/BookingList.vue
+++ src/components/BookingList.vue
@@ -1,18 +1,24 @@
 <template>
-  <section class="grid grid-cols-1 gap-8">
-    <template v-if="!loading">
-      <BookingItem
-        v-for="booking in bookings"
-        :key="booking.id"
-        :title="booking.eventTitle"
-        :status="booking.status"
-        @cancelled="cancelBooking(booking.id)"
-      />
-    </template>
-    <template v-else>
-      <LoadingBookingItem v-for="i in 4" :key="i" />
-    </template>
-  </section>
+  <template v-if="error">
+    <ErrorCard :retry="fetchBookings"> Failed to fetch bookings. Please try again. </ErrorCard>
+  </template>
+
+  <template v-else>
+    <section class="grid grid-cols-1 gap-8">
+      <template v-if="!loading">
+        <BookingItem
+          v-for="booking in bookings"
+          :key="booking.id"
+          :title="booking.eventTitle"
+          :status="booking.status"
+          @cancelled="cancelBooking(booking.id)"
+        />
+      </template>
+      <template v-else>
+        <LoadingBookingItem v-for="i in 4" :key="i" />
+      </template>
+    </section>
+  </template>
 </template>
 
 <script setup>
@@ -20,8 +26,9 @@ import { onMounted } from 'vue'
 import BookingItem from '@/components/BookingItem.vue'
 import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
 import useBookings from '@/composables/useBookings.js'
+import ErrorCard from '@/components/ErrorCard.vue'
 
-const { bookings, loading, fetchBookings, cancelBooking } = useBookings()
+const { bookings, loading, error, fetchBookings, cancelBooking } = useBookings()
 
 onMounted(() => {
   fetchBookings()
